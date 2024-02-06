5701002259acaf7019066e6999c6982ac245fcaf
agusap-git <agusap@gmail.com>
2024-02-06 11:20:38 -0600

    C0001 - Updating legend when opening main gate

--- src/main/java/com/lasvillas/gateservice/service/GateService.java
+++ src/main/java/com/lasvillas/gateservice/service/GateService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class GateService {
     public String openMainGate(String action) {
-        return "Opening Gate....";
+        return "Opening Main Gate....";
     }
 }
